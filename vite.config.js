7708a2f71dde5f7a6f41b9747db756dfa74d882f
itskailash <kailash.wit@gmail.com>
2024-09-17 22:03:14 +0545

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sairose/', // The repository name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist' // Specify the output directory
+  }
+});
